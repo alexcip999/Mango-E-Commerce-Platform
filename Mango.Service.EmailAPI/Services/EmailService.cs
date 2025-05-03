f59c9bd4c74a76081cc509e01407ebaf39c7aa6d
alexcip999 <58331044+alexcip999@users.noreply.github.com>
2025-05-03 11:32:00 +0300

    Email Service Implementation

---
+++ Mango.Service.EmailAPI/Services/EmailService.cs
@@ -0,0 +1,59 @@
+﻿using Mango.Service.EmailAPI.Models;
+using Mango.Services.EmailAPI.Data;
+using Mango.Services.EmailAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Mango.Service.EmailAPI.Services
+{
+    public class EmailService : IEmailService
+    {
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public EmailService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            this._dbOptions = dbOptions;
+        }
+
+        public async Task EmailCartAndLog(CartDto cartDto)
+        {
+            StringBuilder message = new StringBuilder();
+
+            message.AppendLine("<br/>Cart Email Requested ");
+            message.AppendLine("<br/>Total " + cartDto.CartHeader.CartTotal);
+            message.Append("<br/>");
+            message.Append("<ul>");
+            foreach (var item in cartDto.CartDetails)
+            {
+                message.Append("<li>");
+                message.Append(item.Product.Name + " - " + item.Count);
+                message.Append("</li>");
+            }
+            message.Append("</ul>");
+
+            await LogAndEmail(message.ToString(), cartDto.CartHeader.Email);
+        }
+
+        private async Task<bool> LogAndEmail(string message, string email)
+        {
+            try
+            {
+                EmailLogger emailLog = new()
+                {
+                    Email = email,
+                    Message = message,
+                    EmailSent = DateTime.Now
+                };
+                await using var _db = new AppDbContext(_dbOptions);
+                _db.EmailLoggers.AddAsync(emailLog);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
