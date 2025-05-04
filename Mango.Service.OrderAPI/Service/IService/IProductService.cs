3dccaa3553b7919211fbed3459a48f9c04ee52fd
alexcip999 <58331044+alexcip999@users.noreply.github.com>
2025-05-05 00:26:40 +0300

    Checkout UI and Order API

---
+++ Mango.Service.OrderAPI/Service/IService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Service.OrderAPI.Models;
+using Mango.Service.OrderAPI.Models.Dto;
+
+namespace Mango.Services.OrderAPI.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+    }
+}
